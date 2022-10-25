e85f885cc166113fa0163f5eec5cb5e666863d94
Darina Viktorova <darivita1405@gmail.com>
2022-10-25 17:15:16 +0300

    update code...

--- lab_4/app.js
+++ lab_4/app.js
@@ -6,11 +6,13 @@ const jsonParser = express.json();
 
 const userScheme = new Schema({
         name: String,
+        pantronomic: String,
         last_name: String,
         age: Number,
         course: Number,
+        speciality: String,
         avg: Number,
-        department: String
+        city: String
 }, { versionKey: false });
 
 const User = mongoose.model("User", userScheme);
@@ -53,12 +55,14 @@ app.post("/api/users", jsonParser, function (req, res) {
         if (!req.body) return res.sendStatus(400);
 
         const userName = req.body.name;
+        const userPantronomic = req.body.pantronomic;
         const userLastName = req.body.last_name;
         const userAge = req.body.age;
         const userCourse = req.body.course;
+        const userSpeciality = req.body.speciality;
         const userAvg = req.body.avg;
-        const userDepartment = req.body.department;
-        const user = new User({ name: userName, last_name: userLastName, age: userAge, course: userCourse, avg: userAvg, department:userDepartment });
+        const userCity = req.body.city;
+        const user = new User({ name: userName, pantronomic: userPantronomic, last_name: userLastName, age: userAge, course: userCourse, speciality: userSpeciality, avg: userAvg, city: userCity });
 
         user.save(function (err) {
                 if (err) return console.log(err);
@@ -81,15 +85,18 @@ app.put("/api/users", jsonParser, function (req, res) {
         if (!req.body) return res.sendStatus(400);
         const id = req.body.id;
         const userName = req.body.name;
+        const userPantronomic = req.body.pantronomic;
         const userLastName = req.body.last_name;
         const userAge = req.body.age;
         const userCourse = req.body.course;
+        const userSpeciality = req.body.speciality;
         const userAvg = req.body.avg;
-        const userDepartment = req.body.department;
-        const newUser = { name: userName, last_name: userLastName, age: userAge, course: userCourse, avg: userAvg, department:userDepartment };
+        const userCity = req.body.city;
+        const newUser = { name: userName, pantronomic: userPantronomic, last_name: userLastName, age: userAge, course: userCourse, speciality: userSpeciality, avg: userAvg, city: userCity };
 
         User.findOneAndUpdate({ _id: id }, newUser, { new: true }, function (err, user) {
                 if (err) return console.log(err);
                 res.send(user);
         });
 });
+ 
